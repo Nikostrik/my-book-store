bb870f29f527f9eb148aef6cf55bf7cc4381ad52
Nikita Ostrenskyi <nikostrik2003@gmail.com>
2023-10-07 21:03:47 +0300

    second commit

---
+++ src/test/java/project/mybookstore/MyBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package project.mybookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
